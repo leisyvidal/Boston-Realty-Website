0d610e92104be76a163c5298f0d9d328234463bc
Leisy Vidal <leisy.vidal@yahoo.com>
2016-12-07 21:41:18 -0500

    Starter Files
    Starter Files with fonts, images, and header and footer includes files
    linked to index.php.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	$title="Boston Realty";
+	$keywords="Boston real estate, Boston apartments, Boston homes, Boston for rent";
+	$description="We are a realty company based out of Boston, Massachussets looking to help you find your next home.";
+	include('./includes/header.html');
+?>
+    
+<section class="row80">
+    <h1>This is a header 1.</h1>
+    <h2>This is a header 2.</h2>
+    <h3>This is a header 3.</h3>
+    <h4>This is a header 4.</h4>
+    <p>This is a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</section>
+        
+
+<?php
+	include('./includes/footer.html');
+?>
