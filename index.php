c19531210f83becbe6b8975d052c48a38ed1b535
Leisy Vidal <leisy.vidal@yahoo.com>
2016-12-09 12:29:41 -0500

    Home3 Quote Section created
    Home3 Quote Section created and responsive

--- index.php
+++ index.php
@@ -20,14 +20,11 @@
             <h2>How Boston Realty Finds Your Home</h2>
             <p>Boston Realty is the premier realtor in Boston’s greater metropolitan area. We help you find the perfect apartment amongst various MLS listings with tons of research that is specific to your needs and new home requests.</p>
         </div>
-    </div>    
-</section>    
-    
-    <h1>This is a header 1.</h1>
-    <h2>This is a header 2.</h2>
-    <h3>This is a header 3.</h3>
-    <h4>This is a header 4.</h4>
-    <p>This is a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</section>
+
+<section class="row90 home3">
+    <blockquote>“We love the apartment Boston Realty found us. It was the perfect starter home for us newlyweds.” <span>– Mark & Joan Matthews</span></blockquote>
 </section>
         
 
