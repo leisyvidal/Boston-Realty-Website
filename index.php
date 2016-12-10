3bcab66332643cafbf4b063934f289ae44e605bb
Leisy Vidal <leisy.vidal@yahoo.com>
2016-12-10 16:52:32 -0500

    Home4 Section Created
    Home4 Section Created and made responsive

--- index.php
+++ index.php
@@ -26,6 +26,28 @@
 <section class="row90 home3">
     <blockquote>“We love the apartment Boston Realty found us. It was the perfect starter home for us newlyweds.” <span>– Mark & Joan Matthews</span></blockquote>
 </section>
+
+<section class="row80 home4">
+    <h2>Recent Open Houses</h2>
+    <p>These are the top listings in Boston, or better yet, your next future home.</p>
+    
+    <div class="third">
+        <img src="img/photo-1472851342459-470a4dfa8a4b.jpeg" alt="East Park Rd apartment">
+        <p>101 E Park Road</p>
+        <p>$2450 / month</p>
+    </div>
+    <div class="third">
+        <img src="img/photo-1459597093177-5c47509d2d61.jpeg" alt="East Park Rd apartment">
+        <p>53 Church Street</p>
+        <p>$2285 / month</p>
+    </div>
+    
+    <div class="third">
+        <img src="img/photo-1426122402199-be02db90eb90.jpeg" alt="East Park Rd apartment">
+        <p>2431 Evergreen Terrace</p>
+        <p>$3590 / month</p>
+    </div>
+</section>
         
 
 <?php
